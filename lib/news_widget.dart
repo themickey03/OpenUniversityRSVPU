edf93853d6fe2cab4690d702ab21945f63e21b6b
themickey03 <csgomickey03@gmail.com>
2023-03-30 02:03:46 +0500

    get info from server news

--- lib/news_widget.dart
+++ lib/news_widget.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+import 'package:open_university_rsvpu/NewsItemWidget.dart';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class NewsWidget extends StatefulWidget {
   const NewsWidget({Key? key}) : super(key: key);
+
 
   @override
   _WithNewsWidgetState createState() => _WithNewsWidgetState();
@@ -9,6 +14,30 @@ class NewsWidget extends StatefulWidget {
 
 class _WithNewsWidgetState extends State<NewsWidget>
     with AutomaticKeepAliveClientMixin<NewsWidget> {
+
+  final url = "http://45.8.147.95:3333/news";
+
+  var _postsJson = [];
+  void fetchDataNews() async{
+
+    try {
+      final response = await get(Uri.parse(url));
+      final jsonData = jsonDecode(response.body) as List;
+
+      setState(() {
+        _postsJson = jsonData;
+      });
+    } catch (err){
+      print(err);
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    fetchDataNews();
+  }
+
   final items = List<String>.generate(10, (i) => "Новость № $i");
 
   @override
@@ -16,10 +45,20 @@ class _WithNewsWidgetState extends State<NewsWidget>
     return Scaffold(
       body: Center(
         child: ListView.builder(
-          itemCount: items.length,
+          itemCount: _postsJson.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${items[index]}'),
+            return Column(
+              children: [
+                SizedBox(
+                  height: 300,
+                  child: NewsItemWidget(oneNews: OneNews(
+                      _postsJson[index]["title"],
+                      _postsJson[index]["description"],
+                      "http://45.8.147.95:3333/imgs/${_postsJson[index]["img"]["id"]}.${_postsJson[index]["img"]["format"]}",
+                      _postsJson[index]["date"],
+                      _postsJson[index]["views"].toString())),
+                )
+              ],
             );
           },
         ),
