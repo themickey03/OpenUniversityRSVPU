63ca3e316ff4b9d215f60195bd216dd379f102c5
themickey03 <csgomickey03@gmail.com>
2023-03-31 12:40:14 +0500

    news page refresh feature

--- lib/news_widget.dart
+++ lib/news_widget.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -9,18 +10,15 @@ import 'package:open_university_rsvpu/SingleNewsWidget.dart';
 class NewsWidget extends StatefulWidget {
   const NewsWidget({Key? key}) : super(key: key);
 
-
   @override
   _WithNewsWidgetState createState() => _WithNewsWidgetState();
 }
 
 class _WithNewsWidgetState extends State<NewsWidget>
     with AutomaticKeepAliveClientMixin<NewsWidget> {
-
-  final url = "http://45.8.147.95:3333/news";
-
+  final url = "https://koralex.fun/back/news";
   var _postsJson = [];
-  void fetchDataNews() async{
+  void fetchDataNews() async {
 
     try {
       final response = await get(Uri.parse(url));
@@ -29,7 +27,7 @@ class _WithNewsWidgetState extends State<NewsWidget>
       setState(() {
         _postsJson = jsonData;
       });
-    } catch (err){
+    } catch (err) {
       print(err);
     }
   }
@@ -40,112 +38,143 @@ class _WithNewsWidgetState extends State<NewsWidget>
     fetchDataNews();
   }
 
+  Future refresh() async {
+    setState(() {
+      fetchDataNews();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
       body: Center(
-        child: ListView.builder(
-          itemCount: _postsJson.length,
-          itemBuilder: (context, index) {
-
-            var title = "";
-            if (_postsJson[index]["title"] != null) {
-              title = _postsJson[index]["title"];
-            }
-            var description = "";
-            if (_postsJson[index]["description"] != null) {
-              description = _postsJson[index]["description"];
-            }
-            var imagelink = "https://cdn-icons-png.flaticon.com/512/3858/3858629.png";
-            if (_postsJson[index]["img"]["id"] != null && _postsJson[index]["img"]["format"] != null) {
-              imagelink = "http://45.8.147.95:3333/imgs/${_postsJson[index]["img"]["id"]}.${_postsJson[index]["img"]["format"]}";
-            }
-            var publish_date = "";
-            if (_postsJson[index]["date"] != null) {
-              publish_date = _postsJson[index]["date"];
-            }
-            var views = "0";
-            if (_postsJson[index]["views"] != null) {
-              views = _postsJson[index]["views"].toString();
-            }
-            var truePublishDate = "";
-            var dt = DateTime.parse(publish_date);
-            try{
-              truePublishDate = DateFormat('dd.MM.yyyy в kk:mm').format(dt);
-            }
-            catch (err){
-              truePublishDate = publish_date;
-            }
-            return Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Card(
+        child: RefreshIndicator(
+          onRefresh: refresh,
+          child: ListView.builder(
+            itemCount: _postsJson.length,
+            itemBuilder: (context, index) {
+              var title = "";
+              if (_postsJson[index]["title"] != null) {
+                title = _postsJson[index]["title"];
+              }
+              var description = "";
+              if (_postsJson[index]["description"] != null) {
+                description = _postsJson[index]["description"];
+              }
+              var imagelink =
+                  "https://cdn-icons-png.flaticon.com/512/3858/3858629.png";
+              if (_postsJson[index]["img"]["id"] != null &&
+                  _postsJson[index]["img"]["format"] != null) {
+                imagelink =
+                    "https://koralex.fun/back/imgs/${_postsJson[index]["img"]["id"]}.${_postsJson[index]["img"]["format"]}";
+              }
+              var publishDate = "";
+              if (_postsJson[index]["date"] != null) {
+                publishDate = _postsJson[index]["date"];
+              }
+              var views = "0";
+              if (_postsJson[index]["views"] != null) {
+                views = _postsJson[index]["views"].toString();
+              }
+              var truePublishDate = "";
+              var dt = DateTime.parse(publishDate);
+              try {
+                truePublishDate =
+                    DateFormat('dd.MM.yyyy в kk:mm').format(dt);
+              } catch (err) {
+                truePublishDate = publishDate;
+              }
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Card(
                   shadowColor: Colors.black,
                   elevation: 20,
                   child: InkWell(
                     onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SingleNewsWidget(singleNewsModel: new SingleNewsModel(title, imagelink, description, truePublishDate, views),)));
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                            SingleNewsWidget(
+                              singleNewsModel:
+                                SingleNewsModel(
+                                  title,
+                                  imagelink,
+                                  description,
+                                  truePublishDate,
+                                  views),
+                            )));
                     },
                     child: SizedBox(
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width-10.0,
-                              height: (MediaQuery.of(context).size.width-10.0)/16*9,
-                              child: FadeInImage.assetNetwork(
-                                alignment: Alignment.topCenter,
-                                placeholder: 'images/Loading_icon.gif',
-                                image: imagelink,
-                                fit: BoxFit.cover,
-                                width: double.maxFinite,
-                                height: double.maxFinite,
-                              ),
+                      crossAxisAlignment:
+                        CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width:
+                            MediaQuery.of(context).size.width - 10.0,
+                          height:
+                            (MediaQuery.of(context).size.width - 10.0) / 16 * 9,
+                          child: FadeInImage.assetNetwork(
+                            alignment: Alignment.topCenter,
+                            placeholder:
+                              'images/Loading_icon.gif',
+                            image: imagelink,
+                            fit: BoxFit.cover,
+                            width: double.maxFinite,
+                            height: double.maxFinite,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8, right: 8, top: 5),
+                          child: Text(
+                            title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              overflow: TextOverflow.ellipsis),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8, right: 8, top: 5),
+                          child: Text(
+                            description,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 8, top: 5),
-                              child: Text("${title}", style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  overflow: TextOverflow.ellipsis
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 8, top: 5),
-                              child: Text("${description}",
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8, right: 8, top: 5, bottom: 5),
-                              child: Row(
-                                children: [
-                                  Text("Просмотров: ${views}",style: TextStyle(
-                                      color: Colors.grey
-                                  )
-                                  ),
-                                  Spacer(),
-                                  Text(truePublishDate, style: TextStyle(
-                                      color: Colors.grey
-                                  )),
-                                ],
-                              ),
-                            )
-                          ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8,
+                              right: 8,
+                              top: 5,
+                              bottom: 5),
+                          child: Row(
+                            children: [
+                              Text("Просмотров: $views",
+                                  style: const TextStyle(
+                                      color: Colors.grey)),
+                              const Spacer(),
+                              Text(truePublishDate,
+                                  style: const TextStyle(
+                                      color: Colors.grey)),
+                            ],
+                          ),
                         )
-                    ),
-                  )
-              ),
-            );
-          }
-
-        ),
+                      ],
+                    )),
+                  )),
+              );
+            }))
       ),
     );
   }
+
   @override
   bool get wantKeepAlive => true;
 }
