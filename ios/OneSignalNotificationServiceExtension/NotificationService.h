24f783d92c9be5ab261ddb45887fafab85df06f4
themickey03 <csgomickey03@gmail.com>
2023-05-13 16:57:52 +0500

    notification release

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by mickey on 13.05.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
